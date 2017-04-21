12282e7a2ace4174eb2b09d4c57bad1772fbad24
k <brood915@gmail.com>
2017-04-21 10:33:11 -0400

    removed php

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,6 @@ jQuery(function($){
 var BRUSHED = window.BRUSHED || {};
 
 
-
-
-
 /* ==================================================
    Mobile Navigation
 ================================================== */
@@ -186,40 +183,6 @@ BRUSHED.fancyBox = function(){
 		});
 	}
 }
-
-
-/* ==================================================
-   Contact Form
-================================================== */
-
-BRUSHED.contactForm = function(){
-	$("#contact-submit").on('click',function() {
-		$contact_form = $('#contact-form');
-		
-		var fields = $contact_form.serialize();
-		
-		$.ajax({
-			type: "POST",
-			url: "_include/php/contact.php",
-			data: fields,
-			dataType: 'json',
-			success: function(response) {
-				
-				if(response.status){
-					$('#contact-form input').val('');
-					$('#contact-form textarea').val('');
-				}
-				
-				$('#response').empty().html(response.html);
-			}
-		});
-		return false;
-	});
-}
-
-
-
-
 
 /* ==================================================
    Menu Highlight
